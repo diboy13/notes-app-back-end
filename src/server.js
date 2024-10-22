2b6ed84a89e2f31550a603585c066992fbd5ae93
diboy13 <hadidicky31@gmail.com>
2024-10-22 14:01:44 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+console.log("kita akan membuat restful API");
+
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`server ini berjalan pada ${server.info.uri}`);
+};
+
+init();
